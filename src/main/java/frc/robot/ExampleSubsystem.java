33b5db8c5d676ccfd3be435ea6e15ab9d5671796
John Doe <johndoe@example.com>
2025-09-18 23:05:43 -0400

    nre

--- src/main/java/frc/robot/ExampleSubsystem.java
+++ src/main/java/frc/robot/ExampleSubsystem.java
@@ -12,8 +12,10 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+//Use the extends subsytembase to tell the code file that this is a subsystem file and so we can use the methods like periodic
+//Subsystems are used to control motors and read sensors
 public class ExampleSubsystem extends SubsystemBase {
+  //This is where you initialize motors sensors PID controllers and other things you need for your subsystem
     private TalonFX pivot = new TalonFX(1);
     private PIDController pivotPID = new PIDController(0, 0, 0);
     double x = 3;
@@ -28,22 +30,24 @@ public class ExampleSubsystem extends SubsystemBase {
 
    
   
- 
+ // this method allows us to run code one time when the subsystem is created simular to the initialize method in the commands
   public ExampleSubsystem() {
+  
 
 
   }
 
 
 
-  
+  // this method is constintly running after the object is created and can be used to update values on smaertdashboard or other things that need to be updated constintly
   @Override
   public void periodic() {
     SmartDashboard.putNumber("pivot", pivot.getPosition().getValueAsDouble() );
     SmartDashboard.putBoolean("pivot", pivot.getPosition().getValueAsDouble() < 15);
     
   }
-
+//this method is an example of a method that is used to get the position of the motor that has a return type of a double
+//think of this as the hellping methods basicly allowing the command to get information from the subsystem
   public double getPosition(){
     return pivot.getPosition().getValueAsDouble();
     
@@ -51,6 +55,9 @@ public class ExampleSubsystem extends SubsystemBase {
 
 
   }
+
+  //this method is an example of a method that is used to set the speed of the motor that has a parameter of a double and no return type
+  //these void methods can be thought of as the action method basicly allowing the command to tell the subsystem what to do
 
   public void speed1(double speed1){
    
